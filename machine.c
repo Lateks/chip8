4e07ec213db714fb5607a3cb0867d2dcccba1b1f
Laura Leppänen <laura.leppanen@iki.fi>
2018-05-28 20:53:28 +0300

    Implement 8xy0: LD Vx, Vy

--- machine.c
+++ machine.c
@@ -41,6 +41,25 @@ void run_ld_i_addr(struct chip8 *vm, uint16_t instruction) {
     vm->reg_i = MEM_ADDR(instruction);
 }
 
+void run_ld_vx_vy(struct chip8 *vm, uint16_t instruction) {
+    uint8_t reg1 = REG_1(instruction);
+    uint8_t reg2 = REG_2(instruction);
+
+    vm->reg_v[reg1] = vm->reg_v[reg2];
+}
+
+void run_8xyn(struct chip8 *vm, uint16_t instruction) {
+    uint8_t optype = LOW_NIBBLE(instruction);
+
+    switch (optype) {
+        case 0:
+            run_ld_vx_vy(vm, instruction);
+            break;
+        default:
+            printf("Skipping unknown 8xyn instruction %x\n", instruction);
+    }
+}
+
 void vm_run(struct chip8 *vm) {
     uint16_t instruction = read_instruction(vm);
     uint16_t instruction_type = HIGH_NIBBLE(instruction);
@@ -49,13 +68,15 @@ void vm_run(struct chip8 *vm) {
         case 6:
             run_ld_reg_vx(vm, instruction);
             break;
+        case 8:
+            run_8xyn(vm, instruction);
+            break;
         case 0xA:
             run_ld_i_addr(vm, instruction);
             break;
         default:
             printf("Skipping unknown instruction %x\n", instruction);
     }
-
 
     vm->pc = vm->pc + 2;
 }

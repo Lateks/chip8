92df0ea762c92b71aa9d0e75a76e0acba02611df
Laura Leppänen <laura.leppanen@iki.fi>
2018-06-03 09:24:03 +0300

    Restrict redraws to 60fps

--- chip8.c
+++ chip8.c
@@ -8,6 +8,8 @@
 #include "sdl_system.h"
 #include "screen.h"
 
+#define RENDER_INTERVAL_SECONDS 1/60.f
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         puts("Usage: chip8 path/to/rom");
@@ -20,6 +22,7 @@ int main(int argc, char *argv[]) {
     int temp = 0;
     float dt = 1;
     int keypress = -1;
+    float time_since_redraw = RENDER_INTERVAL_SECONDS;
 
     vm_init_with_rom(&vm, argv[1]);
     struct io_state state = init_io(SCREEN_WIDTH_PX, SCREEN_HEIGHT_PX);
@@ -33,13 +36,15 @@ int main(int argc, char *argv[]) {
         }
         vm_run(&vm, dt);
         vm_update_timers(&vm, dt);
-        if (should_redraw(&vm)) {
+        if (should_redraw(&vm) && time_since_redraw >= RENDER_INTERVAL_SECONDS) {
             draw_screen(&state, &vm);
             reset_redraw_flag(&vm);
+            time_since_redraw = 0;
         }
 
         temp = clock();
         dt = (temp - loop_start) / (CLOCKS_PER_SEC * 1.f);
+        time_since_redraw += dt;
         loop_start = temp;
     }
 
